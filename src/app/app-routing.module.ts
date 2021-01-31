7dad0ae1dcf512357d3c4fb87c5ba5155642ec8f
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2021-01-31 16:39:25 +0530

    auth service added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,31 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'places',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthPageModule),
   },
   {
     path: 'places',
-    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./places/places.module').then((m) => m.PlacesPageModule),
   },
   {
     path: 'bookings',
-    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./bookings/bookings.module').then((m) => m.BookingsPageModule),
   },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
