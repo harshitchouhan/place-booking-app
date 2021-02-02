e4bc3640dc47a1215192d1ad2edc71beee6fee35
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2021-02-02 21:37:23 +0530

    sliding options added

---
+++ src/app/bookings/booking.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Booking } from './booking.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookingService {
+  private _bookings: Booking[] = [new Booking('xyz', 'p1', 'abc', 'Manhattan Mansion', 2)];
+
+  constructor() {}
+
+  get booking() {
+    return [...this._bookings];
+  }
+}
